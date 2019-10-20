43ed0e6f99b19507b61517fb106cec4f5aebbceb
Mariasola <mariasolahornedo@gmail.com>
2019-10-20 19:49:49 +0200

    build docs

---
+++ docs/precache-manifest.cb273d5c8569e603d157997470d3ce36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44a20c747b11450c9a5d069d39a2cd2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2d292a0b1152f6a077ff",
+    "url": "./static/css/main.8eeba047.chunk.css"
+  },
+  {
+    "revision": "c792804a78436a0e909c",
+    "url": "./static/js/2.4d54c6f7.chunk.js"
+  },
+  {
+    "revision": "2d292a0b1152f6a077ff",
+    "url": "./static/js/main.f0b816f0.chunk.js"
+  },
+  {
+    "revision": "473618ef79ecb8c5f44d",
+    "url": "./static/js/runtime-main.3c520003.js"
+  },
+  {
+    "revision": "b18d5a38c370a3014b6efd19d48167ba",
+    "url": "./static/media/sleigh.b18d5a38.png"
+  }
+]);
